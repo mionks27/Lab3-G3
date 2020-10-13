ec8019b555a38c057ea043d46321665a4148cc83
Julio Carrion <a20161448@pucp.edu.pe>
2020-10-13 01:20:56 -0500

    avance 1 julio

--- app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -1,8 +1,14 @@
 package pe.edu.pucp.tel306;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import pe.edu.pucp.tel306.Threads.ControladorModelView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Página Principal");
+        ViewModelProvider viewModelProvider = new ViewModelProvider(this);
+        ControladorModelView controladorModelView = viewModelProvider.get(ControladorModelView.class);
+        controladorModelView.getContadorMinutos().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                TextView textView = findViewById(R.id.textViewContadorMinutos);
+                textView.setText(String.valueOf(integer));
+            }
+        });
+
+        controladorModelView.getContadorSegundos().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                TextView textView = findViewById(R.id.textViewContadorSegundos);
+                textView.setText(String.valueOf(integer));
+            }
+        });
     }
+
+    public void iniciarContador (View view){
+        ViewModelProvider viewModelProvider = new ViewModelProvider(this);
+        ControladorModelView controladorModelView = viewModelProvider.get(ControladorModelView.class);
+        controladorModelView.iniciarciclo();
+
+    }
+
 }
---
+++ app/src/main/java/pe/edu/pucp/tel306/Threads/ControladorModelView.java
@@ -0,0 +1,81 @@
+package pe.edu.pucp.tel306.Threads;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+
+public class ControladorModelView extends ViewModel {
+
+    private MutableLiveData<Integer> contadorSegundos = new MutableLiveData<>();
+    private MutableLiveData<Integer> contadorMinutos = new MutableLiveData<>();
+
+    private Thread thread = null;
+
+
+    public void iniciarciclo (){
+        setThread(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int contadorLocalMinutos = 24;
+                int contadorLocalSegundos = 59;
+                while (contadorLocalMinutos > 0){
+                    contadorMinutos.postValue(contadorLocalMinutos);
+                    while (contadorLocalSegundos >= 0){
+                                contadorSegundos.postValue(contadorLocalSegundos);
+                                contadorLocalSegundos = contadorLocalSegundos -1;
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                    break;
+                                }
+                                if(contadorLocalSegundos == 0){
+                                    contadorLocalSegundos = 59;
+                                    break;
+                                }
+                            }
+
+                    contadorLocalMinutos = contadorLocalMinutos -1;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+
+                    if(contadorLocalMinutos == 0){
+                        contadorLocalMinutos = 24;
+                        break;
+                    }
+                }
+
+            }
+        }));
+
+        getThread().start();
+    }
+
+    public MutableLiveData<Integer> getContadorSegundos() {
+        return contadorSegundos;
+    }
+
+    public void setContadorSegundos(MutableLiveData<Integer> contadorSegundos) {
+        this.contadorSegundos = contadorSegundos;
+    }
+
+    public MutableLiveData<Integer> getContadorMinutos() {
+        return contadorMinutos;
+    }
+
+    public void setContadorMinutos(MutableLiveData<Integer> contadorMinutos) {
+        this.contadorMinutos = contadorMinutos;
+    }
+
+    public Thread getThread() {
+        return thread;
+    }
+
+    public void setThread(Thread thread) {
+        this.thread = thread;
+    }
+}
