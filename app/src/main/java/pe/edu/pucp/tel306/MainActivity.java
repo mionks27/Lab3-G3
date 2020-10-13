d823c517712c67a6b1e3a73ab8ec794703a8351d
katty <a20160555@pucp.edu.pe>
2020-10-13 01:26:42 -0500

    HelpButton

--- app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -2,7 +2,10 @@ package pe.edu.pucp.tel306;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void buttonHelp (MenuItem item) {
+        Intent intent = new Intent(MainActivity.this, HelpActivity.class);
+        startActivity(intent);
     }
 }
